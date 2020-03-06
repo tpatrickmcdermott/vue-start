c781ead0c83e786a1fb2d5b3117553559faf4da4
Patrick <dev1@pmcder.com>
2020-03-06 16:26:02 -0500

    1st changes to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
     <EmployeeTable />
-    <hr style="margin-top: 60px">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue';
-  import EmployeeTable from '@/components/EmployeeTable.vue';
+  import EmployeeTable from '@/components/EmployeeTable.vue'
 
   export default {
     name: 'App',
     components: {
-      HelloWorld,
       EmployeeTable
     }
   }
 </script>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
   }
 </style>
